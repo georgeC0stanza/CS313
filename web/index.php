4f354e8581e29a7d2475a80aabe5bec5d6541469
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2019-01-10 11:56:53 -0700

    added hello world

--- web/index.php
+++ web/index.php
@@ -1,5 +1,12 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8"/>
+     <title>David Harris</title>
+   </head>
+   <body>
+    <?php     
+      echo "<p>Hello world</p>"
+    ?>
+   </body>
+</html>
