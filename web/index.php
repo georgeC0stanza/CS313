f7763d74ac3f41a1a56015543ae74f5478930614
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2019-01-19 17:14:30 -0700

    assignment test

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8" http-equiv="Refresh" content="5; url=hello.php"/>
+     <title>David Harris</title>
+   </head>
+   <body>
+    <?php     
+      echo "<p>Hello world</p>"
+    ?>
+   </body>
+</html>
