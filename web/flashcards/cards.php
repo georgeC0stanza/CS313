9652a41535f745f823652041496d48460e8347c9
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2019-02-17 00:08:54 -0700

    bug

--- web/flashcards/cards.php
+++ web/flashcards/cards.php
@@ -63,9 +63,10 @@
 
 <?php
     $user_id = $_session["user_id"];
+    $user_id = 1 ;
 
 //    echo ($user_id);
-    $statement = $db->prepare("SELECT id, cardtext_front, cardtext_back FROM cardset WHERE user_id = 1 ");
+    $statement = $db->prepare("SELECT id, cardtext_front, cardtext_back FROM cardset WHERE user_id = '$user_id' ");
     $statement->execute();
     // Go through each result
     $count_id = 1;
