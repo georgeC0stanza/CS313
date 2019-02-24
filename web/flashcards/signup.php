d3415a7df4a01eb9293eabd36926c42cb6adfbd4
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2019-02-23 23:39:36 -0700

    .

--- web/flashcards/signup.php
+++ web/flashcards/signup.php
@@ -11,32 +11,16 @@
 
     if ($user_password == $user_password2)
     {
-        $query = "SELECT id, FROM person WHERE username = '$user_username'";
+        $hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO person (username, passwrd) VALUES ('$user_username', '$hashed_password')";
         $statement = $db->prepare($query);
         $statement->execute();
-        $user = $statement->fetchAll(PDO::FETCH_ASSOC);
-        
-        $id;
-    
-        foreach ($user as $user_element) {
-            $id = $user_element['id'];
-        }
-
-        if ($id > 0)
-        {
-
-            $hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
-            $query = "INSERT INTO person (username, passwrd) VALUES ('$user_username', '$hashed_password')";
-            $statement = $db->prepare($query);
-            $statement->execute();
-        }
-
     }
     else
     {
-        header('HTTP/1.1 500 Internal Server Error');
+        header('HTTP/1.1 599 Internal Server Error');
         header('Content-Type: application/json; charset=UTF-8');
-        die(json_encode(array('message' => 'INVALID CREDENTIALS', 'code' => 1337)));
+        die(json_encode(array('message' => 'ERROR', 'code' => 0808)));
     }
 ?>
  
